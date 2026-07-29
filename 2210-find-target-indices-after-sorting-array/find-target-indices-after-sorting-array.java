a919e3d1a11146230c3c128de3d25101277dbc9b
NandhiniSakthivelS <nandinisakthivels@gmail.com>
2026-07-29 20:22:37 +0530

    Time: 5 ms (74.48%) | Memory: 46.5 MB (44.72%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -4,6 +4,7 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
+            if(nums[i]>target)break;
             if(nums[i]==target)
             {
                 list.add(i);
