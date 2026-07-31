ec3ef95360b8a33ca9bbea6a06758f0194d19c57
NandhiniSakthivelS <nandinisakthivels@gmail.com>
2026-07-31 14:00:07 +0530

    Time: 4 ms (71.36%) | Memory: 55.3 MB (43.10%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(Character.isLowerCase(ch))
+            {
+                sb.append(ch);
+            }
+            else if(ch=='#')
+            {
+                sb.append(sb);
+            }
+            else if(ch=='%')
+            {
+                sb.reverse();
+            }
+            else if(ch=='*' && sb.length()!=0)
+            {
+                sb.deleteCharAt(sb.length()-1); 
+            }
+        }
+        return sb.toString(); 
+    }
+}
