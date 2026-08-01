bfc403a4eb1e2c91cbe9dcbe2c2b7a1812bcf539
Nandhini Sakthivel <nandinisakthivels@gmail.com>
2026-08-01 10:43:15 +0530

    Time: 3 ms (2.29%) | Memory: 42.2 MB (59.29%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,6 +1,6 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        String binaryStr1 = String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0');
+        /*String binaryStr1 = String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0');
         String binaryStr2 = String.format("%32s", Integer.toBinaryString(y)).replace(' ', '0');
         System.out.println(binaryStr1+" "+binaryStr2);
         int count=0;
@@ -9,6 +9,13 @@ class Solution {
            char ch=binaryStr1.charAt(i);
            char ch2=binaryStr2.charAt(i);
            if(ch!=ch2)count++;
+        }*/
+
+        int ans=x^y;int count=0;
+        String s = String.format("%32s", Integer.toBinaryString(ans)).replace(' ','0');
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='1')count++;
         }
         return count;
     }
