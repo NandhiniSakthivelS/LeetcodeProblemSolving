56a2895954a2cdb8ec56e92c53297376fe029e1b
Nandhini Sakthivel <nandinisakthivels@gmail.com>
2026-08-06 12:32:11 +0530

    Time: 11 ms (39.46%) | Memory: 47.2 MB (20.01%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        char a[]=new char[26];
-        char b[]=new char[26];
-        for(int i=0;i<ransomNote.length();i++)
+        HashMap <Character , Integer> map = new HashMap<>();
+        for(Character ch : magazine.toCharArray())
         {
-            char c1 = ransomNote.charAt(i);
-            a[c1-'a']++;
+            map.merge(ch,1,Integer::sum);
         }
-        for(int i=0;i<magazine.length();i++)
+        for(Character ch : ransomNote.toCharArray())
         {
-            char c2 = magazine.charAt(i);
-            b[c2-'a']++;
-        }
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]>b[i])
+            if(!map.containsKey(ch))return false;
+            else 
             {
-                return false;
+                if(map.get(ch)==0) return false;
+                else map.compute(ch,(k,v)-> v-1);
             }
         }
         return true;
